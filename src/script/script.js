e73a9381eb7b0ed3e91d4c98b412922cdfc663f7
abdisae3 <abdisae3@gmail.com>
2024-04-25 15:30:25 +0700

    commit pertama

---
+++ src/script/script.js
@@ -0,0 +1,2 @@
+document.querySelector('.hamburger').addEventListener('click',()=>{document.querySelector('.nav-link').classList.toggle('expanded');
+document.querySelector('.hamburger>span').classList.toggle('line-top');document.querySelector('.hamburger>span:nth-child(2)').classList.toggle('line-mid');document.querySelector('.hamburger>span:last-child').classList.toggle('line-bot');})
