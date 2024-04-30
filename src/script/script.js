e744817764aae4b5cce670abc52cb643cd66c244
abdisae3 <abdisae3@gmail.com>
2024-04-30 17:04:29 +0700

    revisi beberapa file, terutama navbar

--- src/script/script.js
+++ src/script/script.js
@@ -1,2 +1,7 @@
 document.querySelector('.hamburger').addEventListener('click',()=>{document.querySelector('.nav-link').classList.toggle('expanded');
-document.querySelector('.hamburger>span').classList.toggle('line-top');document.querySelector('.hamburger>span:nth-child(2)').classList.toggle('line-mid');document.querySelector('.hamburger>span:last-child').classList.toggle('line-bot');})
+document.querySelector('.hamburger>span').classList.toggle('line-top');document.querySelector('.hamburger>span:nth-child(2)').classList.toggle('line-mid');document.querySelector('.hamburger>span:last-child').classList.toggle('line-bot');});
+
+/*
+window.addEventListener('DOMContentLoaded',()=>{
+	console.log('Current page path : ', page.filePath);
+});*/
